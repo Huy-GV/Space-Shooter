f4f352e8451390166cb61341af3145decfd920ec
basbaybee <emperorhuyv@gmail.com>
2021-04-18 10:17:32 +1000

    added a gamemode class to handle spawning

---
+++ GameMode.cs
@@ -0,0 +1,16 @@
+using System;
+using SplashKitSDK;
+
+namespace Space_Shooter
+{
+    public class GameMode
+    {
+        public enum Modes
+        {
+            Levels,
+            Survival,
+            BossRun
+        }
+        
+    }
+}
