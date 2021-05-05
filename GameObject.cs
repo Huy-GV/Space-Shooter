ad60dff5acd3e9ceacf1a481058deb05f48c77ed
basbaybee <emperorhuyv@gmail.com>
2021-05-05 17:58:43 +1000

    commit before switching branches

--- GameObject.cs
+++ GameObject.cs
@@ -4,8 +4,6 @@ namespace Space_Shooter
 {
     public abstract class GameObject
     {
-        // public int X{get;  protected set;}
-        // public int Y{get;  protected set;}
         public abstract void Update();
         public abstract void Draw();
     }
