41359ebf1e6e6cfbe80626c70a4e5a58aa81a413
basbaybee <emperorhuyv@gmail.com>
2021-05-24 15:30:53 +1000

    added factory class

---
+++ Factory.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using SplashKitSDK;
+
+namespace SpaceShooter
+{
+    public class EnemyFactory
+    {
+        private List<Enemy> _enemies; 
+        public EnemyFactory(List<Enemy> enemies)
+        {
+            _enemies = enemies;
+        }
+        // public SpawnEnemy
+    }
+}
