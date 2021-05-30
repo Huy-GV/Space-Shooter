0cf53c9eddde2cfc2d51ba402cfaa382bce69dff
basbaybee <emperorhuyv@gmail.com>
2021-05-30 12:07:13 +1000

    added a game object class containing x and y

---
+++ GameObject.cs
@@ -0,0 +1,12 @@
+using SplashKitSDK;
+using System;
+
+namespace SpaceShooter
+{
+    public abstract class GameObject
+    {
+        public int X { get; protected set;}
+        public int Y { get; protected set;}
+
+    }
+}
